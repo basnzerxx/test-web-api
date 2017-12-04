8e7880ed300f20634d847b1696c18c4c7561852c
Nuttanon <56160255@go.buu.ac.th>
2017-12-04 17:57:24 +0700

    new

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ var bparser = require('body-parser')
 var app = express()
 var con = require('../contact')
 
-app.use(bparser.urlencoded({extended:false}))
 app.use(bparser.json())
 
 app.use('/contact',con)
