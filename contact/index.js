ba9ea360ef044f62765a4922d6db1798e516f7a2
Nuttanon <56160255@go.buu.ac.th>
2017-12-04 17:53:13 +0700

    New Time

--- contact/index.js
+++ contact/index.js
@@ -5,9 +5,15 @@ var listName =  [{id:1,Fname :'Nut',Lname:'Su',age:'22',email:'5616@gmail.com'},
 {id:3,Fname :'Ati',Lname:'kom',age:'21',email:'smoo@gmail.com'},
 {id:4,Fname :'panu',Lname:'panpan',age:'23',email:'nuna@gmail.com'},
 {id:5,Fname :'thana',Lname:'kam',age:'21',email:'aonza@gmail.com'},
-{id:6,Fname :'pat',Lname:'tane',age:'20',email:'sword@gmail.com'}];
+{id:6,Fname :'pat',Lname:'tane',age:'20',email:'sword@gmail.com'}]
 
-
+router.get('/', (req, res) => {res.json(listName)});
+//Nuttanon
+router.post('/',(req, res)=> {
+        var bear = req.body; 
+        bear.id=listName.length = listName.push(bear)
+        res.json({message:'Add id +1'});
+    });
 
 
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 var express = require('express')
+var bparser = require('body-parser')
 var app = express()
 var con = require('../contact')
+
+app.use(bparser.urlencoded({extended:false}))
+app.use(bparser.json())
+
 app.use('/contact',con)
 
 module.exports = app
